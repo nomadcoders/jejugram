a9863dd79d0ecc268fe112dddb327fab634e7ac8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-30 16:56:01 +0900

    Overlay 20%;

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
   var $heart = $(".heart");
   $heart.click(function() {
-    var hasHeartClass = $(this).hasClass("heart-clicked");
-    if (hasHeartClass) {
-      $(this).removeClass("fa-heart");
-      $(this).removeClass("heart-clicked");
-      $(this).addClass("fa-heart-o");
-    } else {
-      $(this).removeClass("fa-heart-o");
-      $(this).addClass("fa-heart");
-      $(this).addClass("heart-clicked");
-    }
+    $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
   });
 });
