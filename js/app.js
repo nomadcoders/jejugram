247b31eaa4af3f45076f3e6819894f0334e3d821
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-30 16:41:03 +0900

    js 🙏🏻

--- js/app.js
+++ js/app.js
@@ -1,31 +1,15 @@
-// var weapons = ["AK47", "Magnum", "Kim jong fat"];
-// var soldier = {
-//   name: "Nicolas",
-//   age: 22,
-//   weapon: weapons[2],
-//   isKorean: false,
-//   isDead: false,
-//   sayHi: function() {
-//     console.log("Hello, my name is " + this.name + " and I'm: " + this.age);
-//   }
-// };
-//
-// function multiply(iceCream, horsetato) {
-//   return iceCream * horsetato;
-// }
-//
-// function sum(a, b) {
-//   return a + b;
-// }
-//
-// function substract(a, b) {
-//   return a - b;
-// }
-//
-// function divide(a, b) {
-//   return a / b;
-// }
-//
-// function expo(a, b) {
-//   return a ** b;
-// }
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    var hasHeartClass = $(this).hasClass("heart-clicked");
+    if (hasHeartClass) {
+      $(this).removeClass("fa-heart");
+      $(this).removeClass("heart-clicked");
+      $(this).addClass("fa-heart-o");
+    } else {
+      $(this).removeClass("fa-heart-o");
+      $(this).addClass("fa-heart");
+      $(this).addClass("heart-clicked");
+    }
+  });
+});
