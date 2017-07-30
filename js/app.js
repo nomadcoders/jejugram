f08b6b1646e9e6d6c3d80e23abe52d9b51c4cc96
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-30 15:59:46 +0900

    js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// var weapons = ["AK47", "Magnum", "Kim jong fat"];
+// var soldier = {
+//   name: "Nicolas",
+//   age: 22,
+//   weapon: weapons[2],
+//   isKorean: false,
+//   isDead: false,
+//   sayHi: function() {
+//     console.log("Hello, my name is " + this.name + " and I'm: " + this.age);
+//   }
+// };
+//
+// function multiply(iceCream, horsetato) {
+//   return iceCream * horsetato;
+// }
+//
+// function sum(a, b) {
+//   return a + b;
+// }
+//
+// function substract(a, b) {
+//   return a - b;
+// }
+//
+// function divide(a, b) {
+//   return a / b;
+// }
+//
+// function expo(a, b) {
+//   return a ** b;
+// }
